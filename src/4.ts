f6ad2a5c5ce1f26a97f2472d85585f15d0b519bb
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-11-12 22:42:13 +0200

    ADDED: solutions for tasks 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+interface IKey {
+  getSignature(): number;
+}
+
+interface IPerson {
+  getKey(): Key;
+}
+
+interface IHouse {
+  comeIn(person: Person): void;
+  openDoor(key: Key): void;
+}
+
+class Key implements IKey {
+  protected signature = Math.random() + 1;
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person implements IPerson {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House implements IHouse {
+  protected tentants: Person[] = [];
+  protected door: boolean = false;
+
+  constructor(protected key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tentants.push(person);
+    } else {
+      console.error('Access denied!');
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+    } else {
+      console.error('Access is denied!');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +62,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
